daa0722e1f3b3399af46dcfc693183e5181fe115
Fen2187 <mark.honeyborne@icloud.com>
2026-08-20 13:38:52 +1000

    Student Dashboard development started, global posts being made

--- User/student/dashboard.php
+++ User/student/dashboard.php
@@ -1,3 +1,20 @@
+<?php 
+session_start(); 
+include __DIR__ . '/../../src/db.php';
+
+if(isset($_SESSION['email'])){ 
+    $email = $_SESSION['email']; 
+    
+    $email = mysqli_real_escape_string($conn, $email); 
+    
+    $query = mysqli_query($conn, "SELECT * FROM `students` WHERE email='$email'"); 
+    
+    while($row = mysqli_fetch_array($query)){ 
+        $result = $row['firstName'].' '.$row['lastName']; 
+    } 
+} 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +24,29 @@
     <link rel="stylesheet" href="../../src/style.css">
 </head>
 <body>
+    <header class="container-header">
+
+    </header>
+    <div class="container-lg">
+        <div class="profile">
+            <h1>Hello <?php echo $result;?></h1>
+        </div>
+        <div class="global_posts">
+            <div class="post">
+                <div>
+                    <div class="person">
+                        <h1 class="fname">First Name</h1>
+                        <h1 class="lname">Last Name</h1>
+                    </div>
+                    <div class="person">
+                        <p class=timestamp>Date</p>
+                        <p class="grade">Grade</p>
+                    </div>
+                </div>
+                <p class="message">Message</p>
+            </div>
+        </div>
+    </div>
     
 </body>
 </html>
