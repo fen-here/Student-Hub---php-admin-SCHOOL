386c393e2c92402ad4f3557df00cbba0a53afd03
Fen_Here <129371237+fen-here@users.noreply.github.com>
2026-09-22 11:02:13 +1000

    Add files via upload

---
+++ users.sql
@@ -0,0 +1,146 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 22, 2026 at 12:36 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `users`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(50) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp(),
+  `grade` varchar(50) NOT NULL,
+  `message` varchar(250) NOT NULL,
+  `reacts` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `firstName`, `lastName`, `date`, `grade`, `message`, `reacts`) VALUES
+(3, 'test', 'teacher', '2026-09-21', '10', 'Hello. This is a test message...', 4),
+(4, 'test', 'teacher', '2026-09-21', '9', 'School Esports Team', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(10) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `age` int(100) NOT NULL,
+  `grade` int(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `firstName`, `lastName`, `age`, `grade`, `email`, `password`) VALUES
+(2, 'test', 'user', 16, 10, 'test.user@example.com', '$2y$10$pZ9aBitnrjLQaZSKUEyStuOJLdNlhXsOR6O6JJ2fohVmUifW8WFbm'),
+(22, 'Maddie', 'Smith', 16, 10, 'maddie.smith@school.com', '$2y$10$tYxiPBvtMmtJjVObfiv0u.xMwE/g.AwRcmJIUqjAEs5tIoGh.zrYO'),
+(23, 'Justin', 'Alpha', 12, 6, 'justin.alpha@school.com', '$2y$10$URajCZUBhJcvIhSZYE2Dhuo0Tt0VvzmXrEElMFyr/t3OttpZjtiQu'),
+(24, 'andrew', 'Honeyborne', 15, 9, 'adrew.honeyborne@school.com', '$2y$10$nkJz7VyQYZGV0XDGeQ/dWeASTTtE0upEO9jWdZjwx5FPPl6pL02ES');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `id` int(10) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `age` int(50) NOT NULL,
+  `grade` int(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `teachers`
+--
+
+INSERT INTO `teachers` (`id`, `firstName`, `lastName`, `age`, `grade`, `email`, `password`) VALUES
+(1, 'test', 'teacher', 25, 10, 'test.teacher@example.com', '$2y$10$nQrzRTq.wpKW5xiAoASCgeaWBxmLll3Ak0aBkBLHWJ1qt7.EO6Tye'),
+(18, 'Angus', 'Park', 23, 7, 'angus.park@school.edu.com', '$2y$10$uRb2PZSxTCJ.wLUx/U0MnuVJkJUIjvi0re5CuKySQbNPOPyU1ibb6'),
+(19, 'Ava ', 'Honey', 34, 5, 'ava.honey@school.edu.com', '$2y$10$BLzR3pKvKC.xJw2IBK8Cq.LxuLcAHwXCYosQ.hzSJ/uSSjQ/Nx2Ea');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
